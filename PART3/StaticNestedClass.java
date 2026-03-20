5690808121d5099147b34115688c162fd343a510
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-21 00:45:48 +0530

    Added StaticNestedClass

---
+++ PART3/StaticNestedClass.java
@@ -0,0 +1,13 @@
+public class StaticNestedClass {
+    private int instanceVariable = 10;
+    private static int staticVariable = 20;
+    static class NestedClass{
+        public void print(){
+           // System.out.println(instanceVariable + staticVariable);
+            // This inner nested class can only access the static varialbles of outer class
+            // Hence throws a compile time error 
+        }
+    }
+
+    
+}
