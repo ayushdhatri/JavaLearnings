147133ce63c922462aa9f90d74fb95b1fadb8f60
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-21 20:04:51 +0530

    Added POJO

---
+++ PART3/POJO.java
@@ -0,0 +1,12 @@
+public class POJO {
+    String name;
+    private int rollNumber;
+    protected String address;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
+
