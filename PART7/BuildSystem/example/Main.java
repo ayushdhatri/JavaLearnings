c591308c9b69cb84d543507279384fb2479f14bb
Ayush Solanki <aayushdhatri@gmail.com>
2026-05-12 08:42:21 +0530

    understanding Build System

---
+++ PART7/BuildSystem/example/Main.java
@@ -0,0 +1,13 @@
+package example;
+
+import lib.Hello;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting the code execution....");
+        //Hello.sayHello();
+        Hello.sayHello();
+        System.out.println("Ending the Code Execution");
+    }
+    
+}
---
+++ PART7/BuildSystem/lib/Hello.java
@@ -0,0 +1,8 @@
+package lib;
+
+public class Hello {
+    public static void sayHello(){
+        System.out.println("Hello, Ayush!");
+    }
+    
+}
