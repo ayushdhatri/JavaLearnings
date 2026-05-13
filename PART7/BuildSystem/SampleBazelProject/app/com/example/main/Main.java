6bcf7a1099f63f133a7f2582a15ebc9d7494f766
Ayush Solanki <aayushdhatri@gmail.com>
2026-05-13 16:32:09 +0530

    Improvement in Bzel code

--- PART7/BuildSystem/SampleBazelProject/app/com/example/main/Main.java
+++ PART7/BuildSystem/SampleBazelProject/app/com/example/main/Main.java
@@ -1,10 +1,12 @@
 package com.example.main;
 
-//import com.example.lib.Greeting;
+import Java.JavaLearnings.PART7.BuildSystem.SampleBazelProject.utils.com.example.lib.Greeting;
+
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello from the main class!");
-        //Greeting.sayHello();
+        Greeting.sayHello();
     }
 }
