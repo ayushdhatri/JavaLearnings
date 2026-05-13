51d21e08b9e3abd92c14147299aae3d532ed8e9f
Ayush Solanki <aayushdhatri@gmail.com>
2026-05-13 16:26:08 +0530

    Adding Simple Bazel Project Build steps

---
+++ PART7/BuildSystem/SampleBazelProject/app/com/example/main/Main.java
@@ -0,0 +1,10 @@
+package com.example.main;
+
+//import com.example.lib.Greeting;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello from the main class!");
+        //Greeting.sayHello();
+    }
+}
---
+++ PART7/BuildSystem/SampleBazelProject/utils/com/example/lib/Greeting.java
@@ -0,0 +1,7 @@
+package Java.JavaLearnings.PART7.BuildSystem.SampleBazelProject.utils.com.example.lib;
+
+public class Greeting {
+    public static void sayHello(){
+        System.out.println("Hello from the Greeting class!");
+    }
+}
